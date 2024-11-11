326260b3bc725d23189403ee8de2c00131740b49
Evans Acheampong <evansachie01@gmail.com>
2024-11-11 15:09:10 +0000

    update: add spacing for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,36 +7,34 @@ const Navbar: React.FC = () => {
     <nav className="bg-white py-4 border-b sticky top-0 z-50">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center">
-          <div className="flex items-center space-x-12">
-            <img src={HubtelLogo} alt="Hubtel Logo" className="h-8" />
-            <ul className="flex space-x-8">
-              <li>
-                <Link to="/" className="text-black font-medium hover:text-teal-500 border-b-2 border-teal-500 pb-4">
-                  Make Orders
-                </Link>
-              </li>
-              <li>
-                <Link to="/take-payments" className="text-gray-600 hover:text-teal-500">
-                  Take Payments
-                </Link>
-              </li>
-              <li>
-                <Link to="/grow-revenues" className="text-gray-600 hover:text-teal-500">
-                  Grow Revenues
-                </Link>
-              </li>
-              <li>
-                <Link to="/send-sms" className="text-gray-600 hover:text-teal-500">
-                  Send SMS
-                </Link>
-              </li>
-              <li>
-                <Link to="/serve-with-us" className="text-gray-600 hover:text-teal-500">
-                  Serve With Us
-                </Link>
-              </li>
-            </ul>
-          </div>
+          <img src={HubtelLogo} alt="Hubtel Logo" className="h-8" />
+          <ul className="flex space-x-12">
+            <li>
+              <Link to="/" className="text-black font-medium hover:text-teal-500 border-b-2 border-teal-500 pb-4">
+                Make Orders
+              </Link>
+            </li>
+            <li>
+              <Link to="/take-payments" className="text-gray-600 hover:text-teal-500">
+                Take Payments
+              </Link>
+            </li>
+            <li>
+              <Link to="/grow-revenues" className="text-gray-600 hover:text-teal-500">
+                Grow Revenues
+              </Link>
+            </li>
+            <li>
+              <Link to="/send-sms" className="text-gray-600 hover:text-teal-500">
+                Send SMS
+              </Link>
+            </li>
+            <li>
+              <Link to="/serve-with-us" className="text-gray-600 hover:text-teal-500">
+                Serve With Us
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
